45d72447a7c6fd26992ae02aaa06476dd1f65fe2
Error <foamy125@gmail.com>
2025-10-24 19:29:57 -0500

    nope

--- script/format.ts
+++ script/format.ts
@@ -5,3 +5,11 @@ import { $ } from "bun"
 // Restore original behavior: use the package script named "prettier".
 // (If missing, this will fail as before.)
 await $`bun run prettier --ignore-unknown --write`
+
+if (process.env["CI"] && (await $`git status --porcelain`.text())) {
+  await $`git config --local user.email "action@github.com"`
+  await $`git config --local user.name "GitHub Action"`
+  await $`git add -A`
+  await $`git commit -m "chore: format code"`
+  await $`git push --no-verify`
+}
